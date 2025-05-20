df5ac7f2ca7f6f17fc4ff8325b84417959c4f402
CARLOS URIBURU <carlosuriburupadilla@gmail.com>
2025-05-20 11:31:45 -0300

    Ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("Valor de a="+a);
+Console.WriteLine("Valor de b=" + b);
+
+
+string? n = Console.ReadLine();
+string invertido = "";
+int i = 0;
+
+
+if (int.TryParse(n, out i))
+{
+    if (i > 0)
+    {
+        for (int j = 0; j < n.Length; j++)
+        {
+            invertido = invertido + n[n.Length - j - 1];
+        }
+    }
+
+    Console.WriteLine("El numero invertido es: " + invertido);
+}
