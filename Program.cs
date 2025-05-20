3373acd959104a724f66af230465e6a8bfd4e44f
CARLOS URIBURU <carlosuriburupadilla@gmail.com>
2025-05-20 12:05:03 -0300

    Calculadora V1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
 Console.WriteLine("Hello, World!");
 int a;
 int b;
@@ -7,8 +9,8 @@ b=a;
 Console.WriteLine("Valor de a="+a);
 Console.WriteLine("Valor de b=" + b);
 
-
-string? n = Console.ReadLine();
+Console.WriteLine("Ingrese el numero a invertir");
+string n = Console.ReadLine();
 string invertido = "";
 int i = 0;
 
@@ -24,4 +26,45 @@ if (int.TryParse(n, out i))
     }
 
     Console.WriteLine("El numero invertido es: " + invertido);
+}
+
+
+string op = "";
+
+while (op != "Salir")
+{
+    Console.WriteLine("Seleccione una opcion");
+    Console.WriteLine("\tSumar");
+    Console.WriteLine("\tResta");
+    Console.WriteLine("\tMultiplicacion");
+    Console.WriteLine("\tDivision");
+    Console.WriteLine("\tSalir");
+    op = Console.ReadLine();
+    
+    if (op == "Salir")
+    {
+        break;
+    }
+    Console.WriteLine("Ingrese 2 numeros");
+    int x = 0;
+    int y = 0;
+    if(int.TryParse(Console.ReadLine(),out x) && int.TryParse(Console.ReadLine(),out y)){
+        switch (op)
+        {
+            case "Sumar":
+                Console.WriteLine(x + y);
+                break;
+            case "Resta":
+                Console.WriteLine(x - y);
+                break;
+            case "Multiplicacion":
+                Console.WriteLine(x * y);
+                break;
+            case "Division":
+                Console.WriteLine(x / y);
+                break;
+            default:
+                break;
+        }
+    }
 }
