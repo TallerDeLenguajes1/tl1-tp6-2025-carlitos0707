2c43a13cccdf103a60dafb11590e0d364371090c
CARLOS URIBURU <carlosuriburupadilla@gmail.com>
2025-05-20 12:54:24 -0300

    Calculadora V2

--- Program.cs
+++ Program.cs
@@ -25,3 +25,71 @@ if (int.TryParse(n, out i))
 
     Console.WriteLine("El numero invertido es: " + invertido);
 }
+
+string op = "";
+
+while (op != "Salir")
+{
+    Console.WriteLine("Seleccione una opcion");
+    Console.WriteLine("\tabs");
+    Console.WriteLine("\tCuadrado");
+    Console.WriteLine("\tRaiz");
+    Console.WriteLine("\tseno");
+    Console.WriteLine("\tcoseno");
+    Console.WriteLine("\tParte entera");
+    Console.WriteLine("\tMin");
+    Console.WriteLine("\tMax");
+    Console.WriteLine("\tSalir");
+    op = Console.ReadLine();
+    
+    if (op == "Salir")
+    {
+        break;
+    }
+    
+    int x = 0;
+    int y = 0;
+    float z = 0;
+    bool res;
+    if (op == "Max" || op == "Min")
+    {
+        Console.WriteLine("Ingrese 2 numeros");
+        res = int.TryParse(Console.ReadLine(), out x) && int.TryParse(Console.ReadLine(), out y);
+    }
+    else
+    {
+        Console.WriteLine("Ingrese 1 numero");
+    
+        res = float.TryParse(Console.ReadLine(), out z);
+    }
+    if (res) {
+        switch (op)
+        {
+            case "abs":
+                Console.WriteLine(Math.Abs(z));
+                break;
+            case "Cuadrado":
+                Console.WriteLine(float.Pow(z, 2));
+                break;
+            case "Raiz":
+                Console.WriteLine(float.Sqrt(z));
+                break;
+            case "seno":
+                Console.WriteLine(float.Sin(z));
+                break;
+            case "coseno":
+                Console.WriteLine(float.Cos(z));
+                break;
+            case "Parte entera":
+                Console.WriteLine(Math.Truncate(z));
+                break;
+            case "Max":
+                Console.WriteLine(int.Max(x, y));
+                break;
+            case "Min":
+                Console.WriteLine(int.Min(x, y));
+                break;
+
+        }
+    }
+}
